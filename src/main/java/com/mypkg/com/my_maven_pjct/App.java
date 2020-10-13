7f66a4a2c8851bf7b9fdeaca1d4b6db951e81220
deepak <deepak.aavula@gmail.com>
2020-10-13 15:26:14 +1300

    mvn pprojecr

---
+++ src/main/java/com/mypkg/com/my_maven_pjct/App.java
@@ -0,0 +1,13 @@
+package com.mypkg.com.my_maven_pjct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
